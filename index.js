6690fd06217b4169a83956d660074b4a13568141
Elijah Melis <elimelis@MacBook-Pro.local>
2020-05-17 23:27:58 -0500

    two sum

--- index.js
+++ index.js
@@ -27,3 +27,20 @@ const first_non_repeating_letter = (s) => {
 };
 
 console.log(first_non_repeating_letter("sTreSS")); // returns "T"
+
+/**
+ * Write a function that takes an array of numbers (integers for the tests) and a target number.
+ * It should find two different items in the array that, when added together, give the target value.
+ */
+
+function twoSum(numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+console.log(twoSum([1, 2, 3], 4)); // returns [0,2]
